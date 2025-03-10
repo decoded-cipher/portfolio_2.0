48c254ebc7dfdb7a7c540c9bda8c3cd86abac920
Arjun Krishna <arjun12345krishna@gmail.com>
2025-03-10 21:13:39 +0530

    Vercel Analytics

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
         <div>
+            
             <Helmet>
                 <title>{headerData.name} - Porfolio</title>
             </Helmet>
@@ -24,6 +28,10 @@ function Main() {
             <Blog />
             <Contacts />
             <Footer />
+
+            <Analytics/>
+            <SpeedInsights />
+
         </div>
     )
 }
