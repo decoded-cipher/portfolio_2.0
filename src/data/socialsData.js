60164c9f2883b096953b56691c57f322c4167b2d
Arjun Krishna <arjun12345krishna@gmail.com>
2026-04-30 04:05:43 +0530

    fix: Update RSS link in socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -10,5 +10,5 @@ export const socialsData = {
     // stackOverflow: 'https://stackoverflow.com/users/11622116/decoded-cipher',
     gitlab: 'https://gitlab.com/decoded-cipher',
     // youtube: 'https://youtube.com/'
-    rss: 'https://blog.inovuslabs.org/author/arjun/rss/',
+    rss: 'https://blog.inovuslabs.org/author/arjun',
 }
